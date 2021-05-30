e023bf7f14c0eebfe9e523fe2685beb3f2dcb3bd
wicak231 <muhamadnurwicaksono231@gmail.com>
2021-05-31 01:18:04 +0700

    Pembuatan File index.html

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan GitHub</title>
+</head>
+<body>
+
+	<center>
+		
+		<h1 style="font-size: 40px; font-family: arial;">MUHAMAD NUR WICAKSONO</h1><br>
+		<h2 style="font-size: 25px; font-family: arial;">3193111012</h2>
+
+	</center>
+
+</body>
+</html>
